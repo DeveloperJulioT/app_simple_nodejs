1de2d1e192ed933beffedabf2cb8ff37e90116be
DevJulioT <71162629+DeveloperJulioT@users.noreply.github.com>
2026-09-18 07:33:40 -0500

    Update Git repository and Docker settings

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/DeveloperJulioT/app_simple_nodejs.git', 'master') { node ->
+            node / gitConfigName('DeveloperJulioT')
+            node / gitConfigEmail('julio.david.1338@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('DeveloperJulioT/app_simple_nodejs')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
