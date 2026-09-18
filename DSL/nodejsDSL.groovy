a00adec0916ae6248d4b2d613376bfd39b04e856
DevJulioT <71162629+DeveloperJulioT@users.noreply.github.com>
2026-09-18 07:30:01 -0500

    Change Git repo and user config in nodejsDSL.groovy
    Updated Git repository and user configuration for Node.js DSL job.

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/DeveloperJulioT/app_simple_nodejs.git', 'master') { node ->
+            node / gitConfigName('DeveloperJulioT')
+            node / gitConfigEmail('julio.david.1338@gmail.com')
         }
     }
     triggers {
