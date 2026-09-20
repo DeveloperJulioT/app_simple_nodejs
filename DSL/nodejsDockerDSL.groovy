c66c8b87afb9fdbe624ec3b58fcd4ebf5c9e9382
DevJulioT <71162629+DeveloperJulioT@users.noreply.github.com>
2026-09-19 19:07:28 -0500

    Change Docker repository name for nodejs app

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('DeveloperJulioT/app_simple_nodejs')
+            repositoryName('juliotarquino/app_simple_nodejs')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
